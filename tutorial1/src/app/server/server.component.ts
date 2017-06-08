2ef2287fbb34a4a2a5aaaca21e8f23b6f575f42c
Sreehari Inukollu <isreehari@hotmail.com>
2017-06-08 02:36:37 -0500

    Added databinding.

--- tutorial1/src/app/server/server.component.ts
+++ tutorial1/src/app/server/server.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {    
+
+    serverId: number = 10; 
+    serverStatus: string = 'Offline';
+
+    getServerStatus(){
+        return this.serverStatus;
+    }
+
 }
